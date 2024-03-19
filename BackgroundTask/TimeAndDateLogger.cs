e3b8de13322a8a3c95f46d873bc7460b1b1ec01c
Samson Dada <57550434+Samson-Dada@users.noreply.github.com>
2024-03-19 14:27:45 +0100

    Add Background service implementation

---
+++ BackgroundTask/TimeAndDateLogger.cs
@@ -0,0 +1,70 @@
+﻿namespace ScheduledFileLogger.BackgroundTask
+{
+    public class TimeAndDateLogger
+    {
+        public class LogFileGenerationService : IHostedService
+
+        {
+            private readonly ILogger<LogFileGenerationService> _logger;
+            private readonly string _filePath;
+
+            public LogFileGenerationService(ILogger<LogFileGenerationService> logger, IConfiguration configuration)
+            {
+                _logger = logger;
+
+
+                var projectPath = Directory.GetCurrentDirectory();
+
+
+                _filePath = Path.Combine(projectPath, "ScheduleLog", "Log.txt");
+
+
+                if (!Directory.Exists(Path.GetDirectoryName(_filePath)))
+                {
+                    _ = Directory.CreateDirectory(Path.GetDirectoryName(_filePath));
+                }
+            }
+
+            public async Task StartAsync(CancellationToken stoppingToken)
+            {
+                await WriteLogAsync(stoppingToken);
+            }
+
+            public async Task StopAsync(CancellationToken stoppingToken)
+            {
+                await WriteLogAsync(stoppingToken);
+            }
+
+            private async Task WriteLogAsync(CancellationToken stoppingToken)
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    try
+                    {
+                        var currentDateTime = DateTimeOffset.UtcNow;
+                        var time = currentDateTime.ToLocalTime();
+                        var timestamp = $"Time: {time.TimeOfDay} ---- Date: {currentDateTime:dd/MM/yyyy}";
+
+                        var content = $"Log entry for date and time at: {timestamp}";
+
+                        await WriteToFileAsync(content);
+                        _logger.LogInformation($"File created at: {_filePath}");
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "Error creating file");
+                    }
+
+                    await Task.Delay(TimeSpan.FromSeconds(20), stoppingToken);
+                }
+            }
+
+            private async Task WriteToFileAsync(string content)
+            {
+                using var streamWriter = new StreamWriter(_filePath, true);
+                await streamWriter.WriteLineAsync(content);
+            }
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# ScheduledFileLogger
+## ScheduledFileLogger
+
+-- A .NET web api a background service implemetation using IHostedService 
+
+---it generate a txt log file to a folder and log time and  date in every 20 seconds interval.
+
+-- this approch can be utilize to implement task scheduling,background service and you can use Timer in .NET 
+
+```
+-- but this has nothing to do with cron job or a full fledge background service or scheduled task
+
+
+```
+
+- create folder
+ - create log.txt file
+ - write time and date  to file with file stream
+
+```
+
+
